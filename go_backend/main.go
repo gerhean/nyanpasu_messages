4a9398c67d1d30e751af38ef2b7888ac988a0f69
Ger Hean <gerhean@live.com>
2022-01-30 19:26:01 +0800

    Add some mongo stuff

--- go_backend/main.go
+++ go_backend/main.go
@@ -6,7 +6,53 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var driver_URI string = ""
+
+// This is a user defined method to close resources.
+// This method closes mongoDB connection and cancel context.
+func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+// This is a user defined method that returns mongo.Client,
+// context.Context, context.CancelFunc and error.
+// mongo.Client will be used for further database operation.
+// context.Context will be used set deadlines for process.
+// context.CancelFunc will be used to cancel context and
+// resource associated with it.
+func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+// insertOne is a user defined method, used to insert
+// documents into collection returns result of InsertOne
+// and error if any.
+func insertOne(client *mongo.Client, ctx context.Context,
+	dataBase, col string, doc interface{}) (*mongo.InsertOneResult, error) {
+	// select database and collection ith Client.Database method
+	// and Database.Collection method
+	collection := client.Database(dataBase).Collection(col)
+	// InsertOne accept two argument of type Context
+	// and of empty interface
+	result, err := collection.InsertOne(ctx, doc)
+	return result, err
+}
 
 // album represents data about a record album.
 type boardMessage struct {
@@ -43,9 +89,20 @@ func postMessages(c *gin.Context) {
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/messages", getMessages)
-	router.POST("/messages", postMessages)
+	client, err := mongo.NewClient(options.Client().ApplyURI(driver_URI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
 
-	router.Run("localhost:8080")
+	// router := gin.Default()
+	// router.GET("/messages", getMessages)
+	// router.POST("/messages", postMessages)
+
+	// router.Run("localhost:8080")
 }
