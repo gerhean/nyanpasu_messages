bc37e2490cd89fa0e7e6dba7a820742f714239cc
Ger Hean <gerhean@live.com>
2022-01-31 15:56:40 +0800

    Update vue.config.js
    fix github action problems

--- button_frontend/vue.config.js
+++ button_frontend/vue.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+export const publicPath = process.env.NODE_ENV === 'production'
     ? '/nyanpasu_messages'
-    : '/'
-}
+    : '/';
